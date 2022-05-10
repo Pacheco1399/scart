be0a6959695d2955b3b9da98b2fc67de39914b30
donSantis <santisamaya@gmail.com>
2022-05-10 14:26:34 -0500

    cupones card

--- template/views/cuponesDeTecnologia.php
+++ template/views/cuponesDeTecnologia.php
@@ -1,52 +1,62 @@
 <div class="contenido">
 
-    <div class="component2 gray5 sombra2">
-        <div class="rectangle74">
+    <div class="component2">
+
+
+
+        <div class="logo-content-coupon">
             <a href="#">
-                <img  src="../../assets/img/rectangle74.png" alt="">
+                <img  src="../../assets/img/rectangle7.png" alt="">
             </a>
         </div>
 
-        <div class="rectangle73">
-            <div class="logo-content-coupon">
-                <a href="#">
-                    <img  src="../../assets/img/rectangle7.png" alt="">
-                </a>
+        <div class="share-coupon" >
+            <span class="share-coupon-content">
+            </span>
+            <div class="share-coupon-icon">
+                <i class="share-coupon-icon-vector fas fa-share-alt"></i>
             </div>
+        </div>
 
-            <a class="title-card-coupon"> Suspendisse orci tortor, auctor non luctus </a>
-
-            <a class="content-card-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis facilisis scelerisque nunc, a cursus. </a>
-
-            <div class="button-coupon">
-                <span class="button-show-coupon"><a class="text-button-coupon" href="">Ver Cupon</a> </span>
-            </div>
-
-            <div class="label-sale" >
+        <div class="label-sale" >
             <span class="label-sale-content">
                 <a class="label-sale-text" href="">
                     15% dcto
                 </a>
             </span>
-            </div>
-
-            <div class="time-coupon-container">
-            <span class="time-coupon">
-                <i class="time-coupon-icon fas fa-clock"></i>
-            </span>
-                <a href="" class="text-time-group">Quedan 2 hrs</a>
-
-            </div>
-
-            <div class="share-coupon" >
-                <div class="share-coupon-content">
-                <span class="share-coupon-icon" >
-                    <i class="share-coupon-icon-vector fas fa-share-alt"></i>
-                </span>
-                </div>
-            </div>
-
         </div>
+
+        <div class="button-coupon">
+            <span class="button-show-coupon"></span>
+            <a class="text-button-coupon" href="">Ver Cupón</a>
+        </div>
+
+        <div class="time-coupon-container">
+            <span class="time-coupon">
+                <i class=" time-coupon-icon fas fa-clock"></i>
+            </span>
+            <p class="text-time-group">Quedan 2 hrs</p>
+        </div>
+
+
+
+        <h2 class="description-card-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis facilisis scelerisque nunc, a cursus. </h2>
+
+        <p class="title-card-coupon"> Suspendisse orci tortor, auctor non luctus </p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     </div>
 </div>
