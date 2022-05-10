a18096c94a9fc529d58bfd24df3f2983b9ee8aa9
donSantis <santisamaya@gmail.com>
2022-05-10 15:08:49 -0500

    cupones card

--- template/views/cuponesDeTecnologia.php
+++ template/views/cuponesDeTecnologia.php
@@ -1,9 +1,6 @@
 
 
     <div class="component2">
-
-
-
         <div class="logo-content-coupon">
             <a href="#">
                 <img  src="../../assets/img/rectangle7.png" alt="">
@@ -20,9 +17,9 @@
 
         <div class="label-sale" >
             <span class="label-sale-content">
-                <a class="label-sale-text" href="">
+                <p class="label-sale-text" href="">
                     15% dcto
-                </a>
+                </p>
             </span>
         </div>
 
@@ -32,30 +29,22 @@
         </div>
 
         <div class="time-coupon-container">
+            <p class="text-time-group">Quedan 2 hrs</p>
             <span class="time-coupon">
                 <i class=" time-coupon-icon fas fa-clock"></i>
             </span>
-            <p class="text-time-group">Quedan 2 hrs</p>
+
         </div>
-
-
 
         <h2 class="description-card-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis facilisis scelerisque nunc, a cursus. </h2>
 
         <p class="title-card-coupon"> Suspendisse orci tortor, auctor non luctus </p>
 
-
-
-
-
-
-
-
-
-
-
-
-
+        <span class="imagen-coupon-card">
+            <a href="#">
+                <img  src="../../assets/img/rectangle74.png" alt="">
+            </a>
+        </span>
 
 
     </div>
