3ba246df98b2688c59f826b08f48d18777e06e77
Diego <diegoiretamalp@gmail.com>
2022-05-11 12:18:27 -0400

    msg

--- template/views/banners/botones.php
+++ template/views/banners/botones.php
@@ -1,38 +1,33 @@
 <div class="row">
-    <div class="col-md-2">
-        <button class="btn-todos">
-            <a class="label">TODOS LOS CUPONES</a>
-        </button>
-    </div>
-    <div class="col-md-2">
-        <div class="btn-tecnologia">
-            <img src="../../assets/img/new/tecnologia.png" class="img" alt="tecnologia">
 
-            <a class="label" href="#">TECNOLOGÍA</a>
-        </div>
+    <button class="btn-todos">
+        <a class="label">TODOS LOS CUPONES</a>
+    </button>
+
+    <div class="btn-tecnologia" style="width: 149px">
+        <img src="../../assets/img/new/tecnologia.png" class="img" alt="tecnologia">
+
+        <a class="label" href="#">TECNOLOGÍA</a>
     </div>
-    <div class="col-md-2">
-        <div class="btn-viajes-turismo">
-            <img src="../../assets/img/new/viajes-turismo.png" class="img" alt="viajes-turismo">
-            <a class="label" href="#">VIAJES Y TURISMO</a>
-        </div>
+
+    <div class="btn-tecnologia" style="width: 190px">
+        <img src="../../assets/img/new/viajes-turismo.png" class="img" alt="viajes-turismo">
+        <a class="label" href="#">VIAJES Y TURISMO</a>
     </div>
-    <div class="col-md-2">
-        <div class="btn-bienestar-salud">
-            <img src="../../assets/img/new/bienestar-salud.png" class="img" alt="bienestar-salud">
-            <a class="label" href="#"> BIENESTAR Y SALUD</a>
-        </div>
+
+    <div class="btn-tecnologia" style="width: 199px">
+        <img src="../../assets/img/new/bienestar-salud.png" class="img" alt="bienestar-salud">
+        <a class="label" href="#"> BIENESTAR Y SALUD</a>
     </div>
-    <div class="col-md-2">
-        <div class="btn-deporte">
-            <img src="../../assets/img/new/deportes.png" class="img" alt="deportes">
-            <a class="label" href="#">DEPORTES</a>
-        </div>
+
+    <div class="btn-tecnologia" style="width: 128px">
+        <img src="../../assets/img/new/deportes.png" class="img" alt="deportes">
+        <a class="label" href="#">DEPORTES</a>
     </div>
-    <div class="col-md-2">
-        <div class="btn-hogar">
-            <img src="../../assets/img/new/hogar.png" class="img" alt="hogar">
-            <a class="label" href="#">HOGAR</a>
-        </div>
+
+    <div class="btn-tecnologia" style="width: 106px">
+        <img src="../../assets/img/new/hogar.png" class="img" alt="hogar">
+        <a class="label" href="#">HOGAR</a>
     </div>
+
 </div>
