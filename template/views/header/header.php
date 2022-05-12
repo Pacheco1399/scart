2a2b27b8a0ac9a31d7d67d5cc3aaf7f0ce29d582
Diego <diegoiretamalp@gmail.com>
2022-05-12 03:36:12 -0400

    msg

--- template/views/header/header.php
+++ template/views/header/header.php
@@ -5,32 +5,7 @@
                 <a class="logo navbar-brand" href="/"><img src="../../assets/img/new/logo-blanco.svg"
                                                            alt="Logo de Scart"></a>
 
-                <div class="h-100 ofertas d-md-none">
-                    <a class="ms-0" href="/notificaciones">OFERTAS</a>
-                    <div class="ms-2 numero numero--outline">0</div>
-                </div>
 
-                <div class="h-100 iconos d-md-none">
-                    <a href="/notificaciones" aria-label="Ver mis notificaciones"><img height="18"
-                                                                                       src="../../assets/img/new/icon-campana.svg"
-                                                                                       alt="Ícono de campana"
-                                                                                       aria-hidden="true"/></a>
-                    <a href="/mis-carpetas" aria-label="Ver mis carpetas"><img height="20"
-                                                                               src="../../assets/img/new/icon-corazon.png"
-                                                                               alt="Ícono de corazón"
-                                                                               aria-hidden="true"/></a>
-                    <div class="dropdown">
-                        <a href="/perfil" aria-label="Mostrar menú de usuario" id="header-user-menu-toggler"
-                           data-bs-toggle="dropdown" aria-expanded="false"><img height="20"
-                                                                                src="../../assets/img/new/icon-perfil.png"
-                                                                                alt="Ícono de usuario"
-                                                                                aria-hidden="true"/></a>
-                        <ul class="dropdown-menu" aria-labelledby="header-user-menu-toggler">
-                            <li><a class="dropdown-item" href="/perfil">Ver perfil</a></li>
-                            <li><a class="dropdown-item" href="/cerrarsesion">Cerrar sesión</a></li>
-                        </ul>
-                    </div>
-                </div>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#scart-navbar-menu" aria-controls="scart-navbar-menu" aria-expanded="false"
@@ -94,24 +69,26 @@
                         </form>
                     </div>
                 </div>
-                <div class="col-2 col-sm-1" >
-                    <div class="btn-cupones" >
-                        <a>
-                            <img alt="" src="../../assets/img/new/icon-ticket.png">
-                           <label>CUPONES</label>
+                <div class="col-2 col-sm-1">
+                    <div class="btn-cupones">
+                        <a href="#" class="text">
+                            <img alt=""src="../../assets/img/new/icon-ticket.png">
+
+                            <label >CUPONES</label>
+
                         </a>
 
                     </div>
                 </div>
 
-                <div class="col-2 col-sm-1" style="margin-left: 34px;">
+                <div class="col-2 col-sm-1" style="margin-left: 62px;">
                     <div class="h-100 ofertas">
                         <a class="ms-0 me-2" href="/notificaciones">OFERTAS</a>
                         <div class="numero">0</div>
                     </div>
                 </div>
                 <!--<div class="<? /*=isLoggedIn() ? 'col-6' : 'col-12 col-sm-3'*/ ?> col-lg-2 d-none d-md-block">-->
-                <div class="col-1 col-sm-1" style="margin-left: 20px;">
+                <div class="col-1 col-sm-1" style="margin-left: 62px;">
                     <div class="h-100 iconos">
                         <a href="/notificaciones" aria-label="Ver mis notificaciones"><img height="18"
                                                                                            src="../../assets/img/new/icon-campana.svg"
