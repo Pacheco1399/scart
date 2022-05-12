2b3b1ad51054f7bf3a25220971acb75e6dc3fa5a
donSantis <santisamaya@gmail.com>
2022-05-12 00:09:57 -0500

    responsive

--- template/views/cuponesDeTecnologia.php
+++ template/views/cuponesDeTecnologia.php
@@ -1,221 +1,237 @@
-<hr style="border: 1px solid #BDBDBD; position: absolute; top: 2758px; left: 317px; width: 1286px;">
-
-<label class="title-cupon-tecnologia">CUPONES DE TECNOLOGÍA</label>
 <div class="row">
-    <div class="col-md-6">
-        <div class="coupon-card2">
-
-            <div class="logo-content-coupon-card2">
-                <a href="#">
-                    <img src="../../assets/img/rectangle7.png" alt="">
-                </a>
-            </div>
-
-            <div class="share-coupon-card2">
-            <span class="share-coupon-content-card2">
-            </span>
-                <div class="share-coupon-icon-card2">
-                    <img src="../../assets/img/new/icon-share.png" alt="">
+    <div class="container">
+        <div class="cupones-tecnologia">
+            <div class="container-fluid">
+                <div class="row row-cols-auto row-cols-1 row-cols-md-auto mb-3">
+                    <div class="col">
+                        <h2 class="text-primary-scart gradient-text lh-sm mb-0 landing-title sm">Cupones Scard</h2>
+                    </div>
                 </div>
             </div>
+            <div class="row col-12 ml-2 mb-4">
+                <div class="col-md-6 position-relative">
+                    <div class="coupon-card2">
 
-            <div class="label-sale-card2">
-            <span class="label-sale-content-card2">
-                <p class="label-sale-text-card2" href="">
-                    15% dcto
-                </p>
-            </span>
+                        <div class="logo-content-coupon-card2">
+                            <a href="#">
+                                <img src="../../assets/img/rectangle7.png" alt="">
+                            </a>
+                        </div>
+
+                        <div class="share-coupon-card2">
+                        <span class="share-coupon-content-card2">
+                        </span>
+                            <div class="share-coupon-icon-card2">
+                                <img src="../../assets/img/new/icon-share.png" alt="">
+                            </div>
+                        </div>
+
+                        <div class="label-sale-card2">
+                        <span class="label-sale-content-card2">
+                            <p class="label-sale-text-card2" href="">
+                                15% dcto
+                            </p>
+                        </span>
+                        </div>
+
+                        <div class="button-coupon-card2">
+                            <span class="button-show-coupon-card2"></span>
+                            <a class="text-button-coupon-card2" href="">Ver Cupón</a>
+                        </div>
+
+                        <div class="time-coupon-container-card2">
+                            <p class="text-time-group-card2">Quedan 2 hrs</p>
+                            <span class="time-coupon-card2">
+                            <i class=" time-coupon-icon-card2 fas fa-clock"></i>
+                        </span>
+
+                        </div>
+
+                        <h2 class="description-card2-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis
+                            facilisis scelerisque nunc, a cursus. </h2>
+
+                        <p class="title-card2-coupon"> Suspendisse orci tortor, auctor non luctus </p>
+
+                        <span class="imagen-coupon-card2">
+                        <a href="#">
+                            <img src="../../assets/img/rectangle74.png" alt="">
+                        </a>
+                    </span>
+
+
+                    </div>
+                </div>
+                <div class="col-md-6 position-relative">
+                    <div class="coupon-card2">
+
+                        <div class="logo-content-coupon-card2">
+                            <a href="#">
+                                <img src="../../assets/img/rectangle7.png" alt="">
+                            </a>
+                        </div>
+
+                        <div class="share-coupon-card2">
+                        <span class="share-coupon-content-card2">
+                        </span>
+                            <div class="share-coupon-icon-card2">
+                                <img src="../../assets/img/new/icon-share.png" alt="">
+                            </div>
+                        </div>
+
+                        <div class="label-sale-card2">
+                        <span class="label-sale-content-card2">
+                            <p class="label-sale-text-card2" href="">
+                                15% dcto
+                            </p>
+                        </span>
+                        </div>
+
+                        <div class="button-coupon-card2">
+                            <span class="button-show-coupon-card2"></span>
+                            <a class="text-button-coupon-card2" href="">Ver Cupón</a>
+                        </div>
+
+                        <div class="time-coupon-container-card2">
+                            <p class="text-time-group-card2">Quedan 2 hrs</p>
+                            <span class="time-coupon-card2">
+                            <i class=" time-coupon-icon-card2 fas fa-clock"></i>
+                        </span>
+
+                        </div>
+
+                        <h2 class="description-card2-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis
+                            facilisis scelerisque nunc, a cursus. </h2>
+
+                        <p class="title-card2-coupon"> Suspendisse orci tortor, auctor non luctus </p>
+
+                        <span class="imagen-coupon-card2">
+                        <a href="#">
+                            <img src="../../assets/img/rectangle74.png" alt="">
+                        </a>
+                    </span>
+
+
+                    </div>
+
+                </div>
             </div>
-
-            <div class="button-coupon-card2">
-                <span class="button-show-coupon-card2"></span>
-                <a class="text-button-coupon-card2" href="">Ver Cupón</a>
-            </div>
-
-            <div class="time-coupon-container-card2">
-                <p class="text-time-group-card2">Quedan 2 hrs</p>
-                <span class="time-coupon-card2">
-                <i class=" time-coupon-icon-card2 fas fa-clock"></i>
-            </span>
-
-            </div>
-
-            <h2 class="description-card2-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis
-                facilisis scelerisque nunc, a cursus. </h2>
-
-            <p class="title-card2-coupon"> Suspendisse orci tortor, auctor non luctus </p>
-
-            <span class="imagen-coupon-card2">
-            <a href="#">
-                <img src="../../assets/img/rectangle74.png" alt="">
-            </a>
-        </span>
-
 
         </div>
 
-    </div>
-    <div class="col-md-6">
-        <div class="coupon-card2b">
+        <div class="row">
+            <div class="cupones-tecnologia">
+                <div class="row col-12">
+                    <div class="col-md-6 position-relative">
+                        <div class="coupon-card2">
 
-            <div class="logo-content-coupon-card2">
-                <a href="#">
-                    <img src="../../assets/img/new/icon-apple.png" alt="">
-                </a>
-            </div>
+                            <div class="logo-content-coupon-card2">
+                                <a href="#">
+                                    <img src="../../assets/img/rectangle7.png" alt="">
+                                </a>
+                            </div>
 
-            <div class="share-coupon-card2">
-            <span class="share-coupon-content-card2">
-            </span>
-                <div class="share-coupon-icon-card2">
-                    <img src="../../assets/img/new/icon-share.png" alt="">
-                </div>
-            </div>
+                            <div class="share-coupon-card2">
+                        <span class="share-coupon-content-card2">
+                        </span>
+                                <div class="share-coupon-icon-card2">
+                                    <img src="../../assets/img/new/icon-share.png" alt="">
+                                </div>
+                            </div>
 
-            <div class="label-sale-card2">
-            <span class="label-sale-content-card2">
-                <p class="label-sale-text-card2" href="">
-                    15% dcto
-                </p>
-            </span>
-            </div>
+                            <div class="label-sale-card2">
+                        <span class="label-sale-content-card2">
+                            <p class="label-sale-text-card2" href="">
+                                15% dcto
+                            </p>
+                        </span>
+                            </div>
 
-            <div class="button-coupon-card2">
-                <span class="button-show-coupon-card2"></span>
-                <a class="text-button-coupon-card2" href="">Ver Cupón</a>
-            </div>
+                            <div class="button-coupon-card2">
+                                <span class="button-show-coupon-card2"></span>
+                                <a class="text-button-coupon-card2" href="">Ver Cupón</a>
+                            </div>
 
-            <div class="time-coupon-container-card2">
-                <p class="text-time-group-card2">Quedan 2 hrs</p>
-                <span class="time-coupon-card2">
-                <i class=" time-coupon-icon-card2 fas fa-clock"></i>
-            </span>
+                            <div class="time-coupon-container-card2">
+                                <p class="text-time-group-card2">Quedan 2 hrs</p>
+                                <span class="time-coupon-card2">
+                            <i class=" time-coupon-icon-card2 fas fa-clock"></i>
+                        </span>
 
-            </div>
+                            </div>
 
-            <h2 class="description-card2-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis
-                facilisis scelerisque nunc, a cursus. </h2>
+                            <h2 class="description-card2-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis
+                                facilisis scelerisque nunc, a cursus. </h2>
 
-            <p class="title-card2-coupon"> Suspendisse orci tortor, auctor non luctus </p>
+                            <p class="title-card2-coupon"> Suspendisse orci tortor, auctor non luctus </p>
 
-            <span class="imagen-coupon-card2">
-            <a href="#">
-                <img src="../../assets/img/new/rectangle69.png" alt="">
-            </a>
-        </span>
-
-
-        </div>
-
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-6">
-        <div class="coupon-card2c">
-
-            <div class="logo-content-coupon-card2">
-                <a href="#">
-                    <img src="../../assets/img/new/icon-samsung.png" alt="">
-                </a>
-            </div>
-
-            <div class="share-coupon-card2">
-            <span class="share-coupon-content-card2">
-            </span>
-                <div class="share-coupon-icon-card2">
-                    <img src="../../assets/img/new/icon-share.png" alt="">
-                </div>
-            </div>
-
-            <div class="label-sale-card2">
-            <span class="label-sale-content-card2">
-                <p class="label-sale-text-card2" href="">
-                    15% dcto
-                </p>
-            </span>
-            </div>
-
-            <div class="button-coupon-card2">
-                <span class="button-show-coupon-card2"></span>
-                <a class="text-button-coupon-card2" href="">Ver Cupón</a>
-            </div>
-
-            <div class="time-coupon-container-card2">
-                <p class="text-time-group-card2">Quedan 2 hrs</p>
-                <span class="time-coupon-card2">
-                <i class=" time-coupon-icon-card2 fas fa-clock"></i>
-            </span>
-
-            </div>
-
-            <h2 class="description-card2-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis
-                facilisis scelerisque nunc, a cursus. </h2>
-
-            <p class="title-card2-coupon"> Suspendisse orci tortor, auctor non luctus </p>
-
-            <span class="imagen-coupon-card2">
-            <a href="#">
-                <img src="../../assets/img/new/vista-samsung.png" alt="">
-            </a>
-        </span>
+                            <span class="imagen-coupon-card2">
+                        <a href="#">
+                            <img src="../../assets/img/rectangle74.png" alt="">
+                        </a>
+                    </span>
 
 
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="col-md-6">
-            <div class="coupon-card2d">
+                        </div>
 
-                <div class="logo-content-coupon-card2">
-                    <a href="#">
-                        <img src="../../assets/img/new/icon-lg.png" alt="">
-                    </a>
-                </div>
+                    </div>
+                    <div class="col-md-6 position-relative">
+                        <div class="coupon-card2">
 
-                <div class="share-coupon-card2">
-            <span class="share-coupon-content-card2">
-            </span>
-                    <div class="share-coupon-icon-card2">
-                        <img src="../../assets/img/new/icon-share.png" class="share-coupon-icon-vector-card2" alt="">
+                            <div class="logo-content-coupon-card2">
+                                <a href="#">
+                                    <img src="../../assets/img/rectangle7.png" alt="">
+                                </a>
+                            </div>
+
+                            <div class="share-coupon-card2">
+                        <span class="share-coupon-content-card2">
+                        </span>
+                                <div class="share-coupon-icon-card2">
+                                    <img src="../../assets/img/new/icon-share.png" alt="">
+                                </div>
+                            </div>
+
+                            <div class="label-sale-card2">
+                        <span class="label-sale-content-card2">
+                            <p class="label-sale-text-card2" href="">
+                                15% dcto
+                            </p>
+                        </span>
+                            </div>
+
+                            <div class="button-coupon-card2">
+                                <span class="button-show-coupon-card2"></span>
+                                <a class="text-button-coupon-card2" href="">Ver Cupón</a>
+                            </div>
+
+                            <div class="time-coupon-container-card2">
+                                <p class="text-time-group-card2">Quedan 2 hrs</p>
+                                <span class="time-coupon-card2">
+                            <i class=" time-coupon-icon-card2 fas fa-clock"></i>
+                        </span>
+
+                            </div>
+
+                            <h2 class="description-card2-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis
+                                facilisis scelerisque nunc, a cursus. </h2>
+
+                            <p class="title-card2-coupon"> Suspendisse orci tortor, auctor non luctus </p>
+
+                            <span class="imagen-coupon-card2">
+                        <a href="#">
+                            <img src="../../assets/img/rectangle74.png" alt="">
+                        </a>
+                    </span>
+
+
+                        </div>
+
                     </div>
                 </div>
 
-                <div class="label-sale-card2">
-            <span class="label-sale-content-card2">
-                <p class="label-sale-text-card2" href="">
-                    15% dcto
-                </p>
-            </span>
-                </div>
-
-                <div class="button-coupon-card2">
-                    <span class="button-show-coupon-card2"></span>
-                    <a class="text-button-coupon-card2" href="">Ver Cupón</a>
-                </div>
-
-                <div class="time-coupon-container-card2">
-                    <p class="text-time-group-card2">Quedan 2 hrs</p>
-                    <span class="time-coupon-card2">
-                <i class=" time-coupon-icon-card2 fas fa-clock"></i>
-            </span>
-
-                </div>
-
-                <h2 class="description-card2-coupon"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit a, rutrum turpis
-                    facilisis scelerisque nunc, a cursus. </h2>
-
-                <p class="title-card2-coupon"> Suspendisse orci tortor, auctor non luctus </p>
-
-                <span class="imagen-coupon-card2">
-            <a href="#">
-                <img src="../../assets/img/new/vista-lg.png" alt="">
-            </a>
-        </span>
-
-
             </div>
-
         </div>
+
     </div>
+    <hr class="line-bottom"> </hr>
 </div>
