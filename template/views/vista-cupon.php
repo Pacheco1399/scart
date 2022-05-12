2596e44b061f697e5641b3cb255cc9a23e02944f
donSantis <santisamaya@gmail.com>
2022-05-12 01:13:24 -0500

    responsive

--- template/views/vista-cupon.php
+++ template/views/vista-cupon.php
@@ -59,83 +59,14 @@
 <body class="bg-light user-view">
 
 <main>
-
-    <div class="container" style="width: 1310px">
-        <a href="principal.php" class="vc-ver">
-            <i class="fas fa-angle-left"></i>
-            VER MÁS CUPONES
-
-        </a>
-
-        <hr style="border: 1px solid #BDBDBD; width: 1286px;">
-
-        <label class="vc-label-cupon">
-            CUPÓN DE APPLE
-        </label>
-
-        <div class="vista-cupon-card">
-            <div class="row">
-                <div class="col-md-5">
-
-                    <img src="../../assets/img/new/Rectangle69.png" class="card-img" alt="">
-
-                    <div class="card-share">
-                        <img src="../../assets/img/new/icon-share.png" class="icon" alt="">
-                    </div>
-                    <div class="card-desc">
-                        <label class="text">15% dcto.</label>
-                    </div>
-                </div>
-                <div class="col-md-7">
-                    <img src="../../assets/img/new/icon-apple.png" alt="" class="card-marca-icon">
-                    <label class="vc-card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum
-                        aliquet duis eu tempor in faucibus.</label>
-                    <label class="vc-card-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
-                        tellus, tortor tempus purus. Viverra tincidunt posuere turpis pretium. Sagittis pellentesque
-                        amet, blandit a. Molestie a aliquam non facilisis donec quis aliquam risus donec.</label>
-                    <div class="row">
-                        <div class="vc-card-code-box">
-                            <div class="row">
-
-                                <div class="col-md-4">
-                                    <label class="vc-card-code">SCART#25464</label>
-                                </div>
-                                <div class="col-md-4 offset-md-4">
-                                    <button class="vc-card-code-copy">
-                                        COPIAR
-                                    </button>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <label class="vc-card-subtitle2">Ingresa este código al hacer una comprar en Apple</label>
-
-                    <div class="row">
-                        <div class="col-md-6">
-                            <button class="vc-card-usar">
-                                USAR CUPÓN
-                            </button>
-                        </div>
-                        <div class="col-md-6">
-                            <button class="vc-card-problemas">
-                                PROBLEMAS CON EL CUPÓN
-                            </button>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
+    <div class="row">
+        <!-- <div class="container" style="width: 1310px"> -->
+        <div class="container" style="width: 1286px">
+            <?php include("../views/vista-cupon-card.php") ?>
+            <?php include("../views/carousel2.php") ?>
 
         </div>
-
-        <?php include("../views/carousel2.php") ?>
-
-
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
             crossorigin="anonymous"></script>
