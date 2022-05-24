8f290b18d03da28fede0b0bb12b3a6334d7f9b47
Don Santis <98500973+donSantis@users.noreply.github.com>
2022-05-24 13:02:17 -0500

    card products

---
+++ template/views/products/productCard.php
@@ -0,0 +1,74 @@
+<div class="product-card">
+
+    <div class="header-product-card">
+
+        <div class="share-product-card">
+
+            <div class="botones-share">
+                <div class="dropdown">
+                    <button class="btn compartir" type="button"
+                            id="" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                        <i class="fas fa-share-alt" aria-hidden="true"></i>
+                        <span class="visually-hidden">Compartir ítem en redes sociales</span>
+                    </button>
+                    <ul class="dropdown-menu redes"
+                        aria-labelledby="share-product7641-dropdown">
+                        <li><a class="facebook"
+                               href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fscart.cl%2Fproducto%2F7641-cartera-mujer-tote"
+                               target="_blank"
+                               onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=550,width=600');return false;"
+                               title="Compartir en Facebook" aria-label="Compartir en Facebook"><i
+                                        class="fab fa-facebook-square" aria-hidden="true"></i></a>
+                        </li>
+                        <li><a class="whatsapp"
+                               href="https://api.whatsapp.com/send?text=https%3A%2F%2Fscart.cl%2Fproducto%2F7641-cartera-mujer-tote"
+                               target="_blank"
+                               onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=550,width=600');return false;"
+                               title="Compartir en WhatsApp" aria-label="Compartir en WhatsApp"><i
+                                        class="fab fa-whatsapp-square" aria-hidden="true"></i></a>
+                        </li>
+                        <li><a class="twitter"
+                               href="https://twitter.com/share?url=https%3A%2F%2Fscart.cl%2Fproducto%2F7641-cartera-mujer-tote"
+                               target="_blank"
+                               onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=550,width=600');return false;"
+                               title="Compartir en Twitter" aria-label="Compartir en Twitter"><i
+                                        class="fab fa-twitter-square" aria-hidden="true"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="label-sale-product">
+
+            <p class="label-sale-text-product">
+                15% dcto
+            </p>
+
+        </div>
+
+        <img class="img-product-card" src="../../assets/img/Rectangle85.png" alt="">
+
+    </div>
+
+    <div class="footer-product-card">
+
+
+
+
+    <div class="col-12">
+        <p class="subtitle-product-card"> abcdin </p>
+        <p class="title-product-card"> Mouse inalámbrico M280 </p>
+
+    </div>
+
+
+    <div class="col-12">
+
+    </div>
+
+
+
+</div>
---
+++ template/views/products/prueba.php
@@ -0,0 +1,78 @@
+<div class="product-card">
+    <div class="share-coupon-card2">
+
+        <div class="botones-share">
+            <div class="dropdown">
+                <button class="btn compartir" type="button"
+                        id="" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                    <i class="fas fa-share-alt" aria-hidden="true"></i>
+                    <span class="visually-hidden">Compartir ítem en redes sociales</span>
+                </button>
+                <ul class="dropdown-menu redes"
+                    aria-labelledby="share-product7641-dropdown">
+                    <li><a class="facebook"
+                           href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fscart.cl%2Fproducto%2F7641-cartera-mujer-tote"
+                           target="_blank"
+                           onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=550,width=600');return false;"
+                           title="Compartir en Facebook" aria-label="Compartir en Facebook"><i
+                                class="fab fa-facebook-square" aria-hidden="true"></i></a>
+                    </li>
+                    <li><a class="whatsapp"
+                           href="https://api.whatsapp.com/send?text=https%3A%2F%2Fscart.cl%2Fproducto%2F7641-cartera-mujer-tote"
+                           target="_blank"
+                           onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=550,width=600');return false;"
+                           title="Compartir en WhatsApp" aria-label="Compartir en WhatsApp"><i
+                                class="fab fa-whatsapp-square" aria-hidden="true"></i></a>
+                    </li>
+                    <li><a class="twitter"
+                           href="https://twitter.com/share?url=https%3A%2F%2Fscart.cl%2Fproducto%2F7641-cartera-mujer-tote"
+                           target="_blank"
+                           onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=550,width=600');return false;"
+                           title="Compartir en Twitter" aria-label="Compartir en Twitter"><i
+                                class="fab fa-twitter-square" aria-hidden="true"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    <div class="label-sale-card1">
+
+        <p class="label-sale-text-card1">
+            15% dcto
+        </p>
+
+    </div>
+
+    <img src="../../assets/img/Rectangle70s.jpg" alt="">
+
+    <div class="row">
+        <div class="col-1">
+
+            <i class="time-coupon-icon-card1 far fa-clock"></i>
+
+        </div>
+        <div class="col-11">
+            <p class="text-time-group-card1">Quedan 2 hrs</p>
+        </div>
+    </div>
+    <div class="col-12">
+        <p class="title-card1-coupon"> Suspendisse orci tortor, auctor non luctus </p>
+
+    </div>
+    <div class="col-12">
+        <h2 class="description-card1-coupon">Quamque incidunt, puincidunt, Vestibulum tristique
+            posuere iaculis. Sus.
+        </h2>
+    </div>
+
+
+    <div class="col-12">
+        <div class="button-coupon-card1">
+            <a class="text-button-coupon-card1" href="vista-cupon.php">Ver Cupón</a>
+        </div>
+    </div>
+
+
+</div>
