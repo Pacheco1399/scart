401914a4ed74f00990944112c24a86f910b918e7
Don Santis <98500973+donSantis@users.noreply.github.com>
2022-05-24 23:35:40 -0500

    carousel

---
+++ template/views/banners/banner9.php
@@ -0,0 +1,58 @@
+<div class="carousel-brands mb-5">
+
+    <div class="container">
+
+
+        <div class="carousel-scart">
+            <div class="mb-5">
+                <div class="carousel1__contenedor">
+                    <div class="carousel position-relative carousel1__indicadores landing-carousel">
+                        <button aria-label="Anterior"
+                                id="boton-carousel-new"
+                                class=" carousel1__anterior position-absolute  top-50 start-0 translate-middle-y"
+                                style="z-index: 2">
+                            <i id="icon-carousel" class="fas fa-chevron-left fs-3"></i>
+                        </button>
+                        <div class="glider-wrap px-4">
+                            <div class="carousel1__lista pt-2 pb-1" data-slides-per-page="2"
+                                 data-slides-per-page-sm="3" data-slides-per-page-xl="6">
+
+
+
+
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+                                <?php include("../views/products/cardBrand.php"); ?>
+
+
+
+
+
+
+                            </div>
+                            <button aria-label="Siguiente"
+                                    id="boton-carousel-new"
+                                    class="carousel1__siguiente position-absolute top-50 end-0 translate-middle-y"
+                                    style="z-index: 2">
+
+                                <i id="icon-carousel" class="fas fa-chevron-right fs-3"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        </div>
+    </div>
+</div>
+
---
+++ template/views/products/cardBrand.php
@@ -0,0 +1,3 @@
+<div class="carousel-brands-card">
+        <img class="img-brands-card" src="../../assets/img/b1.png" alt="">
+</div>
