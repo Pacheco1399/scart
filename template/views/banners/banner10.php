40649a27f9d90130982ebd9ad50613051bc72d40
Don Santis <98500973+donSantis@users.noreply.github.com>
2022-05-25 10:33:14 -0500

    carousel

---
+++ template/views/banners/banner10.php
@@ -0,0 +1,21 @@
+<div class="container-fluid relative">
+    <div class="row ">
+        <div class="contact-info-card col-3 flex">
+            <h2>Comunicate con el equipo de SCART</h2>
+            <p>¿Tienes alguna consulta o sugerencia para nosotros? Déjanos tu mensaje y nos pondremos en contacto.</p>
+            <div class="row">
+                <div class="contact-info-fb">
+                    <i class="fa-brands fa-facebook"></i>
+                    <h3 class="flex">holas</h3>
+                </div>
+            </div>
+        </div>
+        <div class="contact-card col-6" >
+
+        </div>
+
+
+
+
+    </div>
+</div>
