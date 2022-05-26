68b80d36a1ed59c31466b30633e651879a59e8f2
Don Santis <98500973+donSantis@users.noreply.github.com>
2022-05-26 03:29:07 -0500

    RESPALDO

--- template/views/banners/banner10.php
+++ template/views/banners/banner10.php
@@ -1,60 +1,71 @@
-<div class="row">
+<div class="row  ">
+    <div class=" col-2 align-self-start">
+        <div class="row">
 
-    <div class="con-card-redes">
-        <label class="title">Comunicate con el equipo de SCART</label>
-        <p class="subtitle">¿Tienes alguna consulta o sugerencia para nosotros?
-            Déjanos tu mensaje y nos pondremos en contacto.</p>
-        <div class="row">
-            <div class="col-xl-2 mt-2">
-                <img src="../../assets/img/new/redes-03.png" alt="">
-            </div>
-            <div class="col-xl-4">
-                <label class="rs">Facebook</label>
-                <p class="rs-s">@Scart</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xl-2 mt-2">
-                <img src="../../assets/img/new/redes-02.png" alt="">
-            </div>
-            <div class="col-xl-4">
-                <label class="rs">Twitter</label>
-                <p class="rs-s">@SCARTcl</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xl-2 mt-2">
-                <img src="../../assets/img/new/redes-01.png" alt="">
-            </div>
-            <div class="col-xl-4">
-                <label class="rs">Instagram</label>
-                <p class="rs-s">@scart.cl</p>
-            </div>
+        <div class="col-8 "></div>
         </div>
     </div>
 
-    <div class="con-card">
 
-        <div class="content">
-            <div class="col-xl-12">
-                <div class="con-icon">
-                    <label class="text">Your name
-                        <input class="con-name form-control" type="text" placeholder="Full name here">
-                    </label>
+    <div class="col-10 align-self-end">
+        <div class="con-card  col align-self-end">
+            <div class="row">
+                <div class="col-4 ">
+                    <div class="con-card-redes col-4 align-self-start ">
+                        <label class="title">Comunicate con el equipo de SCART</label>
+                        <p class="subtitle">¿Tienes alguna consulta o sugerencia para nosotros?
+                            Déjanos tu mensaje y nos pondremos en contacto.</p>
+                        <div class="row">
+                            <div class="col-xl-2 mt-2">
+                                <img src="../../assets/img/new/redes-03.png" alt="">
+                            </div>
+                            <div class="col-xl-4">
+                                <label class="rs">Facebook</label>
+                                <p class="rs-s">@Scart</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-2 mt-2">
+                                <img src="../../assets/img/new/redes-02.png" alt="">
+                            </div>
+                            <div class="col-xl-4">
+                                <label class="rs">Twitter</label>
+                                <p class="rs-s">@SCARTcl</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-2 mt-2">
+                                <img src="../../assets/img/new/redes-01.png" alt="">
+                            </div>
+                            <div class="col-xl-4">
+                                <label class="rs">Instagram</label>
+                                <p class="rs-s">@scart.cl</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="content col-6 ">
+                    <div class="col-xl-12 col-12">
+                        <div class="con-icon">
+                            <label class="text">Your name
+                                <input class="con-name form-control" type="text" placeholder="Full name here">
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-xl-12">
+                        <label class="text">Your mail
+                            <input class="con-mail form-control" type="email" placeholder="Your email address">
+                        </label>
+                    </div>
+                    <div class="col-xl-12">
+                        <label class="text">Message
+                            <textarea class="con-message form-control" placeholder="Type your message here"></textarea>
+                        </label>
+                    </div>
+                    <button type="submit" class="con-btn"><p class="f-flex justify-content-center">Enviar Mensaje
+                        <p></p></button>
                 </div>
             </div>
-            <div class="col-xl-12">
-                <label class="text">Your mail
-                    <input class="con-mail form-control" type="email" placeholder="Your email address">
-                </label>
-            </div>
-            <div class="col-xl-12">
-                <label class="text">Message
-                    <textarea class="con-message form-control" placeholder="Type your message here"></textarea>
-                </label>
-            </div>
-            <button type="submit" class="con-btn"><p class="f-flex justify-content-center">Enviar Mensaje
-                <p></p></button>
         </div>
     </div>
 
