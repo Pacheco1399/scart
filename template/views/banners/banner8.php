07e804a29a09e60bf9d86dc24d8714515219e327
Don Santis <98500973+donSantis@users.noreply.github.com>
2022-05-26 16:14:26 -0500

    correcion responsive card categorias

--- template/views/banners/banner8.php
+++ template/views/banners/banner8.php
@@ -1,4 +1,7 @@
 <div class="banner8 container-fluid col-12 ">
+
+    <!--
+
     <div class="row">
         <div class="col-6">
             <div class="row">
@@ -82,6 +85,98 @@
         </div>
 
     </div>
+
+
+    -->
+
+    <div class="row">
+        <div class="carousel-scart relative">
+            <div class="mb-5">
+                <div class="carousel1__contenedor">
+                    <div class="carousel position-relative carousel1__indicadores landing-carousel">
+                        <button aria-label="Anterior"
+                                id="boton-carousel-new-prev"
+                                class=" carousel1__anterior position-absolute  top-50  translate-middle-y"
+                                style="z-index: 2">
+                            <i id="icon-carousel" class="fas fa-chevron-left fs-3"></i>
+                        </button>
+                        <div class="glider-wrap px-4">
+                            <div class="carousel1__lista pt-2 pb-1" data-slides-per-page-md="1">
+
+                                <div class="card-brand-section">
+                                        <div class="content-card-brand">
+                                            <img src="../../assets/img/sqr4.png" alt="">
+                                            <h3 class="title-card-brand"> Proin Odio Cursus </h3>
+                                            <p class="p-card-brand">Cursus scelerisque mattis ac duis varius facilisi tortor.
+                                                Pellentesque erat nec ornare proin commodo amet blandit. Iaculis pharetra convallis
+                                                etiam.</p>
+                                        </div>
+
+
+
+                                </div>
+                                <div class="card-brand-section">
+                                    <div class="content-card-brand">
+                                        <img src="../../assets/img/sqr4.png" alt="">
+                                        <h3 class="title-card-brand"> Proin Odio Cursus </h3>
+                                        <p class="p-card-brand">Cursus scelerisque mattis ac duis varius facilisi tortor.
+                                            Pellentesque erat nec ornare proin commodo amet blandit. Iaculis pharetra convallis
+                                            etiam.</p>
+                                    </div>
+
+
+
+                                </div>
+                                <div class="card-brand-section">
+                                    <div class="content-card-brand">
+                                        <img src="../../assets/img/sqr4.png" alt="">
+                                        <h3 class="title-card-brand"> Proin Odio Cursus </h3>
+                                        <p class="p-card-brand">Cursus scelerisque mattis ac duis varius facilisi tortor.
+                                            Pellentesque erat nec ornare proin commodo amet blandit. Iaculis pharetra convallis
+                                            etiam.</p>
+                                    </div>
+
+
+
+                                </div>
+                                <div class="card-brand-section">
+                                    <div class="content-card-brand">
+                                        <img src="../../assets/img/sqr4.png" alt="">
+                                        <h3 class="title-card-brand"> Proin Odio Cursus </h3>
+                                        <p class="p-card-brand">Cursus scelerisque mattis ac duis varius facilisi tortor.
+                                            Pellentesque erat nec ornare proin commodo amet blandit. Iaculis pharetra convallis
+                                            etiam.</p>
+                                    </div>
+
+
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <button aria-label="Siguiente"
+                                id="boton-carousel-new-next"
+                                class="carousel1__siguiente position-absolute top-50 translate-middle-y"
+                                style="z-index: 2">
+                            <i id="icon-carousel" class="fas fa-chevron-right fs-3"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
     <div class="col-12">
         <?php include("../views/banners/banner9.php"); ?>
     </div>
