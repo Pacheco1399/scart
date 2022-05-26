b2123be53faecb6603dc3d742ac00d5694f432c6
Pacheco1399 <diegoignacioretamal99@gmail.com>
2022-05-26 10:45:07 -0400

    msg

--- template/views/banners/banner10.php
+++ template/views/banners/banner10.php
@@ -1,16 +1,17 @@
 <div class="row  ">
-    <div class=" col-2 align-self-start">
+    <div class="col-12 col-xl-2 align-self-start">
         <div class="row">
 
-        <div class="col-8 "></div>
+            <div class="col-8 "></div>
         </div>
     </div>
 
 
-    <div class="col-10 align-self-end">
+    <div class="col-12 col-xl-10 align-self-end">
         <div class="con-card  col align-self-end">
             <div class="row">
-                <div class="col-4 ">
+
+                <div class="col-xl-4">
                     <div class="con-card-redes col-6 align-self-start ">
                         <label class="title">Comunicate con el equipo de SCART</label>
                         <p class="subtitle">¿Tienes alguna consulta o sugerencia para nosotros?
@@ -44,27 +45,27 @@
                         </div>
                     </div>
                 </div>
-                <div class="content col-6 ">
+
+                <div class="content col-8 offset-4 ">
                     <div class="col-xl-12 col-12">
-                        <div class="con-icon">
-                            <label class="text">Your name
-                                <input class="con-name form-control" type="text" placeholder="Full name here">
-                            </label>
-                        </div>
+                        <p class="text">Your name
+                            <input class="con-name form-control" type="text" placeholder="Full name here">
+                        </p>
                     </div>
                     <div class="col-xl-12">
-                        <label class="text">Your mail
+                        <p class="text">Your mail
                             <input class="con-mail form-control" type="email" placeholder="Your email address">
-                        </label>
+                        </p>
                     </div>
                     <div class="col-xl-12">
-                        <label class="text">Message
+                        <p class="text">Message
                             <textarea class="con-message form-control" placeholder="Type your message here"></textarea>
-                        </label>
+                        </p>
                     </div>
                     <button type="submit" class="con-btn"><p class="f-flex justify-content-center">Enviar Mensaje
                         <p></p></button>
                 </div>
+
             </div>
         </div>
     </div>
